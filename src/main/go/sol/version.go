8aac537afe297eac6e9b1a29126d596d105ba9b8
SR-G <serge.simon@gmail.com>
2017-12-20 13:19:36 +0100

    Switching to 1.0.3-SNAPSHOT, #17

--- src/main/go/sol/version.go
+++ src/main/go/sol/version.go
@@ -13,7 +13,7 @@ type version struct {
 }
 
 // Version string
-var Version = version{"sleep-on-lan", 1, 0, 2, "SNAPSHOT", ""}
+var Version = version{"sleep-on-lan", 1, 0, 3, "SNAPSHOT", ""}
 
 // Build string
 var Build string
