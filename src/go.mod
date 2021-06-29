60e4e16efcc63ebd4bb6291ca57191da8fcc722d
SR-G <serge.simon@gmail.com>
2021-06-29 22:32:16 +0200

    #10, allow --config to load another configuration filename (nothing else is changed = without this parameter, file alongside the binary will be picked up)

--- src/go.mod
+++ src/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Microsoft/go-winio v0.5.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-ping/ping v0.0.0-20210506233800-ff8be3320020
+	github.com/integrii/flaggy v1.4.4
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- src/sol.go
+++ src/sol.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/integrii/flaggy"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -9,37 +10,67 @@ import (
 
 var configuration = Configuration{}
 var configurationFileName = "sol.json"
+var configurationFileNameFromCommandLine string
+
+func init() {
+	// Init flag reader
+	flaggy.String(&configurationFileNameFromCommandLine, "c", "config", "Configuration file to use (optional, default is 'sol.json' next to the binary)")
+	flaggy.SetName("Sleep-On-LAN")
+	flaggy.SetDescription("Daemon allowing to send a linux or windows computer to sleep")
+	flaggy.DefaultParser.ShowHelpOnUnexpected = true
+	flaggy.SetVersion(Version.Version())
+	flaggy.Parse()
+
+	// Init loggers
+	PreInitLoggers()
+}
 
 func main() {
+	// Check which configuration file to use (either from --config, either sol.json file alongside the binary, either default values)
+	var fullConfigurationFileName string
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	PreInitLoggers()
-	var fullConfigurationFileName = dir + string(os.PathSeparator) + configurationFileName
+
+	if configurationFileNameFromCommandLine != "" {
+		if _, err := os.Stat(configurationFileNameFromCommandLine); err == nil {
+			Info.Println("Will use configuration file provided through --config parameter, path is [" + configurationFileNameFromCommandLine + "]")
+			fullConfigurationFileName = configurationFileNameFromCommandLine
+		} else {
+			Warning.Println("Configuration file provided through --config parameter not found on disk, path is [" + configurationFileNameFromCommandLine + "], will try default value")
+			fullConfigurationFileName = dir + string(os.PathSeparator) + configurationFileName
+		}
+	} else {
+		fullConfigurationFileName = dir + string(os.PathSeparator) + configurationFileName
+	}
+
+	// Loads configuration
 	configuration.InitDefaultConfiguration()
 	configuration.Load(fullConfigurationFileName)
 	configuration.Parse()
 	Info.Println("Application [" + Version.ApplicationName + "], version [" + Version.Version() + "]")
-	// Info.Println("Loaded configuration from [" + fullConfigurationFileName + "]")
 
+	// Display found IP/MAC
 	Info.Println("Now starting sleep-on-lan, hardware IP/mac addresses are : ")
 	for key, value := range LocalNetworkMap() {
 		Info.Println(" - local IP adress [" + key + "], mac [" + value + "]")
 	}
 
+	// Display commands found in configuration
 	Info.Println("Available commands are : ")
 	for _, command := range configuration.Commands {
 		Info.Println(" - operation [" + command.Operation + "], command [" + command.Command + "], default [" + strconv.FormatBool(command.IsDefault) + "], type [" + command.CommandType + "]")
 	}
 
+	// Starts listeners, per configuration
 	for _, listenerConfiguration := range configuration.listenersConfiguration {
 		if listenerConfiguration.active {
 			if strings.EqualFold(listenerConfiguration.nature, "UDP") {
 				go ListenUDP(listenerConfiguration.port)
 			} else if strings.EqualFold(listenerConfiguration.nature, "HTTP") {
-				go ListenHTTP(listenerConfiguration.port) // , configuration.Commands, configuration.Auth, configuration.HTTPOutput)
+				go ListenHTTP(listenerConfiguration.port)
 			}
 		}
 	}
 
-	// Info.Println("sleep-on-lan up and running")
-	select {} // block forever
+	// Blocks forever
+	select {}
 }
